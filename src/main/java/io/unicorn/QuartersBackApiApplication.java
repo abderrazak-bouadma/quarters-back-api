faeb914126fbbbdbe5353b42a4f10b03fe29406c
Abderrazak BOUADMA <bouadma.abderrazak@gmail.com>
2017-04-10 23:14:30 +0200

    BORN

---
+++ src/main/java/io/unicorn/QuartersBackApiApplication.java
@@ -0,0 +1,12 @@
+package io.unicorn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartersBackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartersBackApiApplication.class, args);
+	}
+}
---
+++ src/main/java/io/unicorn/client/GitHubClient.java
@@ -0,0 +1,14 @@
+package io.unicorn.client;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+
+/**
+ * Created by Abderrazak BOUADMA
+ * on 10/04/2017.
+ */
+
+@FeignClient(name = "https://api.github.com")
+public interface GitHubClient {
+
+    
+}
