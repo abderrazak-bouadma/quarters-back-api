123ae5d7c1c2f867b9f12e5205a2f06b470ee754
Abderrazak BOUADMA <bouadma.abderrazak@gmail.com>
2017-04-12 00:26:20 +0200

    fixing load feign clients

--- src/main/java/io/unicorn/service/GitHubServiceClient.java
+++ src/main/java/io/unicorn/service/GitHubServiceClient.java
@@ -2,12 +2,10 @@ package io.unicorn.service;
 
 import io.unicorn.service.dto.Issue;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@Service
 @FeignClient(name = "qb-issues", url = "https://api.github.com")
 public interface GitHubServiceClient {
 
--- src/main/java/io/unicorn/web/rest/QuartersBackController.java
+++ src/main/java/io/unicorn/web/rest/QuartersBackController.java
@@ -3,6 +3,7 @@ package io.unicorn.web.rest;
 import io.unicorn.service.GitHubServiceClient;
 import io.unicorn.service.dto.Issue;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1")
+@EnableFeignClients(clients = {GitHubServiceClient.class})
 public class QuartersBackController {
 
     @Autowired
