bc847f3782527a37c4509232ab3a2dc00f6655bd
Abderrazak BOUADMA <bouadma.abderrazak@gmail.com>
2017-04-11 11:06:24 +0200

    start scritp

--- run.sh
+++ run.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
+# pull the latest code from master
+checkout() {
+    git checkout master
+    git pull
+}
+
+# builds the master branch
 build() {
     mvn clean install
 }
 
+# starts the app
 start() {
     java -Dspring.profiles.active=dev,swagger \
         -jar target/*.jar
@@ -11,5 +19,6 @@ start() {
 
 # ----
 
+checkout
 build
 start
